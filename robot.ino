b30fbff0629ab8fdf9c726f7a7b0fe08244ca7ff
xayon40-12 <touroux.nathan@gmail.com>
2015-11-12 17:56:56 +0100

    Create robot.ino

---
+++ robot.ino
@@ -0,0 +1,39 @@
+int mAvant = 3;
+int mArrier = 4;
+int angle = 5;
+int data = 0;
+
+void setup(){
+  Serial.begin(9600);
+  analogWrite(angle);
+  digitalWrite(mAvant, LOW);
+  digitalWrite(mArrier, LOW);
+}
+
+void loop(){
+  if (Serial.available() > 0) {
+    data = Serial.read();
+    switch(data){
+      case 0:
+        digitalWrite(mAvant, LOW);
+        digitalWrite(mArrier, LOW);
+        break;
+      case 1:
+        digitalWrite(mAvant, HIGH);
+        digitalWrite(mArrier, LOW);
+        break;
+      case 2:
+        digitalWrite(mAvant, LOW);
+        digitalWrite(mArrier, HIGH);
+        break;
+      case 3:
+        while(Serial.available() <= 0)
+          delay(1);
+        data = Serial.read();
+        analogWrite(angle, data);
+        break;
+      default:break; 
+    }
+    analogWrite(angle);
+  }
+}
