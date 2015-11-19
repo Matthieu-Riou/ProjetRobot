acea09868706e93319553968e69d4e33ddbca146
Matthieu-Riou <matthieu.riou@etu.univ-nantes.fr>
2015-11-19 18:10:17 +0100

    Update

--- script/core/arduino/robot/robot.ino
+++ script/core/arduino/robot/robot.ino
@@ -9,13 +9,14 @@ void setup(){
   analogWrite(angle, 0);
   pinMode(mAvant, OUTPUT);
   pinMode(mArrier, OUTPUT);
-  digitalWrite(mAvant, LOW);
+  digitalWrite(mAvant, HIGH);
   digitalWrite(mArrier, LOW);
+  Serial.println("Go !");
 }
 
 void loop(){
   if (Serial.available()) {
-    data = Serial.read()-'0';
+    data = Serial.read();
     Serial.println(data);
     switch(data){
       case 0:
@@ -33,10 +34,13 @@ void loop(){
       case 3:
         while(Serial.available() <= 0)
           delay(1);
-        data = Serial.read() - '0';
+        data = Serial.read();
+        Serial.println(data);
         analogWrite(angle, data);
         break;
-      default:break; 
+      default:
+        Serial.println('Error');
+        break; 
     }
     delay(1);
   }
