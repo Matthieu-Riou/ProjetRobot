5e36042bc26054a125a02f8faa0daa3e1030edb3
Matthieu-Riou <matthieu.riou@etu.univ-nantes.fr>
2015-11-26 18:40:16 +0100

    Add servomotor

--- script/core/arduino/robot/robot.ino
+++ script/core/arduino/robot/robot.ino
@@ -1,16 +1,54 @@
+#include <Servo.h>
+
 int mAvant = 5;
 int mArrier = 6;
-int angle = 9;
+int servoPin = 9;
+int feedbackPin = A0;
 int data = 0;
+
+Servo myservo;
+
+// Calibration values
+int minDegrees;
+int maxDegrees;
+int minFeedback;
+int maxFeedback;
+int tolerance = 2; // max feedback measurement error
+ 
+/*
+This function establishes the feedback values for 2 positions of the servo.
+With this information, we can interpolate feedback values for intermediate positions
+*/
+void calibrate(Servo servo, int analogPin, int minPos, int maxPos)
+{
+  // Move to the minimum position and record the feedback value
+  servo.write(minPos);
+  minDegrees = minPos;
+  delay(2000); // make sure it has time to get there and settle
+  minFeedback = analogRead(analogPin);
+  // Move to the maximum position and record the feedback value
+  servo.write(maxPos);
+  maxDegrees = maxPos;
+  delay(2000); // make sure it has time to get there and settle
+  maxFeedback = analogRead(analogPin);
+}
 
 void setup(){
   Serial.begin(9600);
-  pinMode(angle, OUTPUT);
-  analogWrite(angle, 0);
+  Serial.println("Moteur");
   pinMode(mAvant, OUTPUT);
   pinMode(mArrier, OUTPUT);
   digitalWrite(mAvant, HIGH);
   digitalWrite(mArrier, LOW);
+  Serial.println("Servo-moteur");
+  myservo.attach(servoPin);
+  calibrate(myservo, feedbackPin, 20, 160); // calibrate for the 20-160 degree range
+  Serial.begin(9600);
+  Serial.println("20 : " + minFeedback);
+  Serial.println("160 : " + maxFeedback);
+  myservo.write(90);
+  delay(2000);
+  Serial.println("90 : " + analogRead(feedbackPin));
   Serial.println("Go !");
 }
 
@@ -36,7 +74,7 @@ void loop(){
           delay(1);
         data = Serial.read();
         Serial.println(data);
-        analogWrite(angle, data);
+        myservo.write(data);
         break;
       default:
         Serial.println('Error');
